e7623186b31b950deb05908bdc7fa2faafa02271
unknown <shankhantanoli1@gmail.com>
2022-05-23 19:38:14 +0500

    New Update

---
+++ app/Http/Livewire/Admin/Dashboard/Events/Slots/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Dashboard\Events\Slots;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.dashboard.events.slots.index');
+    }
+}
